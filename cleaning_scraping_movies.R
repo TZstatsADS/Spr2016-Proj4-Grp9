d0f0bfebd9d812f3a371812dc2e854e9a133f9f7
Yue Shen Gu <yueshengu14@gmail.com>
2016-04-12 22:28:05 -0400

    update

--- cleaning_scraping_movies.R
+++ cleaning_scraping_movies.R
@@ -30,10 +30,57 @@ getAmazonInfo<-function(asin){
   }))
 }
 
-system.time(amzData1<-getAmazonInfo(levels(rawMovies$product_productid)[1:100])) #111.65sec 2min ; 110days
+#system.time(amzData1<-getAmazonInfo(levels(rawMovies$product_productid)[1:100])) #111.65sec 2min ; 110days
+system.time(amzData1<-getAmazonInfo(popularMovies)) #111.65sec 2min ; 110days
 amzData1
 
-library(reader)
-movietxt1<-readLines("C:/Users/ygu/Desktop/columbia/movies/movies_000001.txt") #1013628
-movietxt2<-n.readLines("C:/Users/ygu/Desktop/columbia/movies.txt",n=1000000,skip=1013628) #1013628
-a<-read.table(file="C:/Users/ygu/Desktop/columbia/movies.txt",sep=':')
+movietxt<-NULL
+for(i in 1:72){
+  cat(i,'\n')
+  if(i<=9)
+    movietxt<-
+      c(movietxt,
+        readLines(paste0("C:/Users/ygu/Desktop/columbia/movies/movies_00000",i,".txt")))
+  else
+    movietxt<-
+      c(movietxt,
+        readLines(paste0("C:/Users/ygu/Desktop/columbia/movies/movies_0000",i,".txt")))
+}
+# movietxt1<-readLines("C:/Users/ygu/Desktop/columbia/movies/movies_000001.txt") #1013628
+# movietxt2<-readLines("C:/Users/ygu/Desktop/columbia/movies.txt") #1013628
+# a<-read.table(file="C:/Users/ygu/Desktop/columbia/movies.txt",sep=':')
+tail(movietxt) #38732978
+length(movietxt)
+movietxt[38732978:38732981]
+
+productId=movietxt[1:4303664*9-8]
+userId=movietxt[1:4303664*9-7]
+helpfulness=movietxt[1:4303664*9-5]
+score=movietxt[1:4303664*9-4]
+time=movietxt[1:4303664*9-3]
+summary=movietxt[1:4303664*9-2]
+text=movietxt[1:4303664*9-1]
+
+movieDataFrame<-cbind(productId,userId,helpfulness,score,time,summary,text)
+movieDataFrame[,'productId']<-gsub('product\\/productId\\: ','',movieDataFrame[,'productId'])
+movieDataFrame[,'userId']<-gsub('review\\/userId\\: ','',movieDataFrame[,'userId'])
+movieDataFrame[,'helpfulness']<-gsub('review\\/helpfulness\\: ','',movieDataFrame[,'helpfulness'])
+movieDataFrame[,'score']<-gsub('review\\/score\\: ','',movieDataFrame[,'score'])
+movieDataFrame[,'time']<-gsub('review\\/time\\: ','',movieDataFrame[,'time'])
+movieDataFrame[,'summary']<-gsub('review\\/summary\\: ','',movieDataFrame[,'summary'])
+movieDataFrame[,'text']<-gsub('review\\/text\\: ','',movieDataFrame[,'text'])
+
+save(movieDataFrame,file="D:/movieDataFrame.R")
+
+gsub('product\\/productId\\: ','',"product/productId: B003AI2VGA")
+
+product id
+user id
+profilename
+helpfulness
+score
+time
+summary
+text
+
+as.Date(as.POSIXct(1182729600, origin="1970-01-01"))
